895bad56216eadc475869af599a3d10eb3878f5b
John Kennedy <JHKennedy4@gmail.com>
2013-04-21 00:36:07 -0400

    something

--- TeleTracker/views.py
+++ TeleTracker/views.py
@@ -2,7 +2,6 @@ from flask import render_template
 from TeleTracker import TeleTracker
 from cartodb import CartoDBAPIKey, CartoDBException
 import simplejson as json
-app = Flask(__name__)
 user =  'jhkennedy4@gmail.com'
 API_KEY ='c3c310cb4bf016cd634e4df3d0a88b82826a4fbb'
 cartodb_domain = 'jhk'
