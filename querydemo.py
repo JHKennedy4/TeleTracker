259978a8d180cb8299662acdb88e203477c9e570
Wyatt Winters <git@wyattwinters.com>
2013-04-20 23:26:33 -0400

    datum sounds good

--- querydemo.py
+++ querydemo.py
@@ -1,6 +1,5 @@
 from cartodb import CartoDBAPIKey, CartoDBException
 import httplib2
-httplib2.Http(disable_ssl_certificate_validation=True)
 user =  'jhkennedy4@gmail.com'
 API_KEY ='c3c310cb4bf016cd634e4df3d0a88b82826a4fbb'
 cartodb_domain = 'jhk'
