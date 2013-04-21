3b619aa1b619d581a0ee8342e1d89720c60aeb6f
Wyatt Winters <git@wyattwinters.com>
2013-04-20 23:41:33 -0400

    Printing json in browser

--- teletracker.py
+++ teletracker.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from cartodb import CartoDBAPIKey, CartoDBException
-import json
+import simplejson as json
 app = Flask(__name__)
 user =  'jhkennedy4@gmail.com'
 API_KEY ='c3c310cb4bf016cd634e4df3d0a88b82826a4fbb'
@@ -10,8 +10,8 @@ cl = CartoDBAPIKey(API_KEY, cartodb_domain)
 
 @app.route("/")
 def hello():
-    return cl.sql('select * from teletracker where cartodb_id = 2')
-
+	data = cl.sql('select * from teletracker where cartodb_id = 2')
+	return json.dumps(data, indent = 2)
 
 if __name__ == "__main__":
     app.run()
